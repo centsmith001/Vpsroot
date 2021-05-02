ab2e6615060643212b56df4c7be205459616161a
centsmith001 <79389709+centsmith001@users.noreply.github.com>
2021-05-02 16:08:01 +0800

    Add files via upload

---
+++ vpsroot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+clear                                             
+echo -e '+======================================================================+'
+echo -e '+                                                                      +'
+echo -e '+           To set password just run "passwd root" comman              +'
+echo -e '+ Go to your VPS credentials or "identities" if you are using juicessh +'          
+echo -e '+         Set your username to "root" then input your password"        +'
+echo -e '+                                                                      +'
+echo -e '+======================================================================+'
+systemctl restart ssh
+rm -f root.sh
+exit 1
